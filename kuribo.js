e4f771b28e9844fac5d9df8c373503530a80e173
Iván Saavedra <ivykairos@gmail.com>
2021-07-02 14:01:08 -0500

    first commit

---
+++ kuribo.js
@@ -0,0 +1,8 @@
+var node = document.createElement("DIV");
+var textnode = document.createTextNode("fhfjgfjvfwjgfuiewgaufu");
+node.appendChild(textnode);
+node.style.background = "blue";
+node.style.position = "absolute"
+node.style.width = "60px";
+node.style.height = "100px";
+document.body.appendChild(node);
